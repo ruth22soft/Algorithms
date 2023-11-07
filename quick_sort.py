87b80d1a68144f20f19ce8a17c80cabccf356790
ruth22soft <ruthtamiru10@gmail.com>
2023-11-07 08:18:46 +0300

    stack with the ascending order

--- quick_sort.py
+++ quick_sort.py
@@ -1,92 +1,50 @@
-# Python program for implementation of MergeSort
-
-# Merges two subarrays of arr[].
-# First subarray is arr[l..m]
-# Second subarray is arr[m+1..r]
 from timeit import default_timer as timer
-import random 
+import random
 start = timer()
-def merge(arr, l, m, r):
-	n1 = m - l + 1
-	n2 = r - m
+# Function to find the partition position
+def partition(array, low, high):
+	# choose the rightmost element as pivot
+	pivot = array[high]
+	# pointer for greater element
+	i = low - 1
+	# traverse through all elements
+	# compare each element with pivot
+	for j in range(low, high):
+		if array[j] <= pivot:
+			# If element smaller than pivot is found
+			# swap it with the greater element pointed by i
+			i = i + 1
+			# Swapping element at i with element at j
+			(array[i], array[j]) = (array[j], array[i])
+	# Swap the pivot element with the greater element specified by i
+	(array[i + 1], array[high]) = (array[high], array[i + 1])
+	# Return the position from where partition is done
+	return i + 1
 
-	# create temp arrays
-	L = [0] * (n1)
-	R = [0] * (n2)
-
-	# Copy data to temp arrays L[] and R[]
-	for i in range(0, n1):
-		L[i] = arr[l + i]
-
-	for j in range(0, n2):
-		R[j] = arr[m + 1 + j]
-
-	# Merge the temp arrays back into arr[l..r]
-	i = 0	 # Initial index of first subarray
-	j = 0	 # Initial index of second subarray
-	k = l	 # Initial index of merged subarray
-
-	while i < n1 and j < n2:
-		if L[i] <= R[j]:
-			arr[k] = L[i]
-			i += 1
-		else:
-			arr[k] = R[j]
-			j += 1
-		k += 1
-
-	# Copy the remaining elements of L[], if there
-	# are any
-	while i < n1:
-		arr[k] = L[i]
-		i += 1
-		k += 1
-
-	# Copy the remaining elements of R[], if there
-	# are any
-	while j < n2:
-		arr[k] = R[j]
-		j += 1
-		k += 1
-
-# l is for left index and r is right index of the
-# sub-array of arr to be sorted
+def quickSort(array, low, high):
+	if low < high:
+		# Find pivot element such that
+		# element smaller than pivot are on the left
+		# element greater than pivot are on the right
+		pi = partition(array, low, high)
+		# Recursive call on the left of pivot
+		quickSort(array, low, pi - 1)
+		# Recursive call on the right of pivot
+		quickSort(array, pi + 1, high)
 
 
-def mergeSort(arr, l, r):
-	if l < r:
-
-		# Same as (l+r)//2, but avoids overflow for
-		# large l and h
-		m = l+(r-l)//2
-
-		# Sort first and second halves
-		mergeSort(arr, l, m)
-		mergeSort(arr, m+1, r)
-		merge(arr, l, m, r)
-
-
-# Driver code to test above
 arr = []
-for a in range(1,10):
-    n = random.randint(1,10)
+for a in range(1,5000):
+    n = random.randint(1,5000)
     arr.append(n)
 #this is to generate the array in increasing order(best case)
-arr = sorted(arr)
+#arr = sorted(arr)
 #this is to generate an array in decreasing order(worst case)
-arr.sort(reverse=True)
-#print(arr)    
-#print(array)
-n = len(arr)
+#arr.sort(reverse=True)
 
+size = len(arr)
 
-mergeSort(arr, 0, n-1)
-for i in range(n):
-	print("%d" % arr[i],end=" ")
-
+quickSort(arr, 0, size - 1)
+print(arr)
 end = timer()
-
-
 print(end-start)
-
-
